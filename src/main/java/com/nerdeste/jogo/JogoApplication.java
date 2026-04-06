b50548e3c0a3665736ba75f952b6b71b3551e247
HorusGustavo <l.gustavoprofissionall@gmail.com>
2026-04-06 00:28:50 -0300

    Adicionar endpoint de teste

--- src/main/java/com/nerdeste/jogo/JogoApplication.java
+++ src/main/java/com/nerdeste/jogo/JogoApplication.java
@@ -2,6 +2,8 @@ package com.nerdeste.jogo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class JogoApplication {
@@ -11,3 +13,14 @@ public class JogoApplication {
 	}
 
 }
+
+
+@RestController
+class TesteController {
+    
+    @GetMapping("/api/teste")
+    public String teste() {
+        return "BACKEND FUNCIONANDO!";
+    }
+}
+
